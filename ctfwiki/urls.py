f761f2c284e9362a55654fdd55cf3c1dfc4551b8
k.eii <68538183+jonscafe@users.noreply.github.com>
2025-03-10 02:25:28 +0000

    Add static file serving in development and custom admin base template

--- ctfwiki/urls.py
+++ ctfwiki/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,3 +8,6 @@ urlpatterns = [
     path('', include('wiki.urls')),
     path('accounts/', include('django.contrib.auth.urls')),  # login, logout
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
